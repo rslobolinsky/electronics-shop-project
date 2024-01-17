7d20aded09f1c118bd895493bd205a78b3b15e9e
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-01-17 14:41:41 +0400

    add_functions

--- src/item.py
+++ src/item.py
@@ -13,7 +13,18 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = None
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
+    def __repr__(self):
+        return (f"{self.__name} privat name\n"
+                f"{self.name} not privat name\n"
+                f"P{self.price} price\n"
+                f"{self.quantity} quantity\n"
+                f"{self.all} all items\n")
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +32,45 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
+    @property
+    def name(self) -> str:
+        """
+        Get name
+        """
+        return f"{self.__name}"
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """
+        Inout new name
+        """
+        self.__name = str(name).strip()[:10].capitalize()
+
+    @classmethod
+    def instantiate_from_csv(cls, path_file: str) -> None:
+        """
+        Get csv file and create 5 classes
+        """
+        cls.all.clear()
+
+        with open(path_file, 'r', encoding='windows-1251') as csv_file:
+            file = csv.DictReader(csv_file)
+
+            for row in file:
+                cls(row['name'], float(row['price']), float(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string: str) -> int:
+        """
+        Change from strint to float
+        """
+        clean_string = string.strip().replace(',', '.')
+        return int(float(clean_string))
