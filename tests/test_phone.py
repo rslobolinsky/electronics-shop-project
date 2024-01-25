68c381621aacf51e1a073c1fcb5e1b5d7d91e7d0
Ramzes_13 <rslobolinsky13@gmail.com>
2024-01-25 15:32:43 +0400

    add tests for "test_phone.ru"

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -5,3 +5,18 @@ def test_get_number_of_sim():
     phone = Phone("iPhone 14", 120_000, 5, 2)
 
     assert phone.number_of_sim == 2
+
+def test_set_number_of_sim():
+    phone = Phone("iPhone 14", 120_000, 5, 2)
+
+    # Valid value
+    phone.number_of_sim = 3
+    assert phone.number_of_sim == 3
+
+    # Float value
+    phone.number_of_sim = 1.5
+    assert phone.number_of_sim == 3
+
+    # Value is below zero
+    phone.number_of_sim = -1
+    assert phone.number_of_sim == 3
