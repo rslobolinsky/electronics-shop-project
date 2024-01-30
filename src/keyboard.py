fce24623173976c3e11afb6f1b844890dda2b414
Ramzes_13 <rslobolinsky13@gmail.com>
2024-01-30 08:03:45 +0400

    fix code

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,17 +2,16 @@ from src.item import Item
 
 
 class Mixin:
+    _language = 'EN'
 
-    def __init__(self):
-        self.language = None
-
-    def install_lang(self):
-        self._language = 'EN'
+    @property
+    def language(self):
+        return self._language
 
     def change_lang(self) -> None:
         if self._language == 'EN':
             self._language == 'RU'
-        elif self._language == 'RU':
+        else:
             self._language == 'EN'
 
 
@@ -20,11 +19,38 @@ class Keyboard(Item, Mixin):
 
     def __init__(self, name: str, price: float, quantity: int) -> object:
         super().__init__(name, price, quantity)
-        Mixin.install_lang(self)
 
     def __repr__(self):
         return f"Keyboard({self.name}, {self.price}, {self.quantity}, {self._language})"
 
-    @property
-    def language(self):
-        return self._language
+
+
+
+
+# class Mixin:
+#
+#     def __init__(self):
+#         self.language = None
+#
+#     def install_lang(self):
+#         self._language = 'EN'
+#
+#     def change_lang(self) -> None:
+#         if self._language == 'EN':
+#             self._language == 'RU'
+#         elif self._language == 'RU':
+#             self._language == 'EN'
+#
+#
+# class Keyboard(Item, Mixin):
+#
+#     def __init__(self, name: str, price: float, quantity: int) -> object:
+#         super().__init__(name, price, quantity)
+#         Mixin.install_lang(self)
+#
+#     def __repr__(self):
+#         return f"Keyboard({self.name}, {self.price}, {self.quantity}, {self._language})"
+#
+#     @property
+#     def language(self):
+#         return self._language
